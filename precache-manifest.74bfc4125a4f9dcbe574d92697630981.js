83977dbb98a739e9fc3a087ebadc28265471ecd0
unknown <dejan.obr@hotmail.com>
2020-07-21 12:18:46 +0200

    Updates

---
+++ precache-manifest.74bfc4125a4f9dcbe574d92697630981.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd504530d61599c1b14eb5202cd46b82",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6eed0497584abc1f0534",
+    "url": "/monster-rolodex/static/css/main.8e0f8195.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eed0497584abc1f0534",
+    "url": "/monster-rolodex/static/js/main.77ae1d8a.chunk.js"
+  },
+  {
+    "revision": "212570b3a5f291f8b8f3",
+    "url": "/monster-rolodex/static/js/runtime-main.4a41f6c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
